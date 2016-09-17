94d7dd163822abb7fdd5259339b50848b2f21e2c
Kelvin FAN <comp192716s2@gmail.com>
2016-09-18 00:28:49 +1000

    Update

--- View/HunterView.c
+++ View/HunterView.c
@@ -1,25 +1,47 @@
 // HunterView.c ... HunterView ADT implementation
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <string.h>
 #include "Globals.h"
 #include "Game.h"
 #include "GameView.h"
 #include "HunterView.h"
-// #include "Map.h" ... if you decide to use the Map ADT
+#include "Map.h" 
      
 struct hunterView {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    int hello;
+     Map g;
+     GameView gameView;
+     PlayerMessage *ms;
 };
      
 
 // Creates a new HunterView to summarise the current state of the game
 HunterView newHunterView(char *pastPlays, PlayerMessage messages[])
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
+    assert(pastPlays != NULL);
+    assert(messages != NULL);
+
     HunterView hunterView = malloc(sizeof(struct hunterView));
-    hunterView->hello = 42;
+    assert(hunterView != NULL);
+    hunterView->g = newMap();
+    assert(hunterView->g != NULL); 
+    hunterView->gameView = newGameView(pastPlays, messages);
+    assert(hunterView->gameView != NULL); 
+    
+    int turn = getRound(hunterView->gameView) * NUM_PLAYERS + getCurrentPlayer(hunterView->gameView); 
+    if(turn > 0) {
+        hunterView->ms = malloc((turn + 4) * sizeof(char *));
+        assert(hunterView->ms != NULL);
+        
+        int i;
+        for(i = 0; i < turn; i++) {
+            //assert(hunterView->ms[i] != NULL);
+            strcpy(hunterView->ms[i], messages[i]);
+        }
+    }
+
     return hunterView;
 }
      
@@ -27,8 +49,20 @@ HunterView newHunterView(char *pastPlays, PlayerMessage messages[])
 // Frees all memory previously allocated for the HunterView toBeDeleted
 void disposeHunterView(HunterView toBeDeleted)
 {
-    //COMPLETE THIS IMPLEMENTATION
-    free( toBeDeleted );
+    assert(toBeDeleted != NULL);
+    assert(toBeDeleted->g != NULL);
+    assert(toBeDeleted->gameView != NULL);
+
+    
+    int turn = getRound(toBeDeleted->gameView) * NUM_PLAYERS + getCurrentPlayer(toBeDeleted->gameView);  
+    if(turn > 0) {
+        assert(toBeDeleted->ms != NULL);  
+        free(toBeDeleted->ms); 
+    }
+
+    disposeMap(toBeDeleted->g);   
+    disposeGameView(toBeDeleted->gameView); 
+    free(toBeDeleted);
 }
 
 
@@ -37,36 +71,31 @@ void disposeHunterView(HunterView toBeDeleted)
 // Get the current round
 Round giveMeTheRound(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getRound(currentView->gameView);
 }
 
 // Get the id of current player
 PlayerID whoAmI(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getCurrentPlayer(currentView->gameView);
 }
 
 // Get the current score
 int giveMeTheScore(HunterView currentView)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getScore(currentView->gameView);
 }
 
 // Get the current health points for a given player
 int howHealthyIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getHealth(currentView->gameView, player);
 }
 
 // Get the current location id of a given player
 LocationID whereIs(HunterView currentView, PlayerID player)
 {
-    //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return 0;
+    return getLocation(currentView->gameView, player);
 }
 
 //// Functions that return information about the history of the game
