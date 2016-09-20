0b7903171373b10d383c5ff885466dff6010fadf
MrTomT <z5118143@zmail.unsw.edu.au>
2016-09-20 10:10:53 +1000

    Update DracView.c

--- View/DracView.c
+++ View/DracView.c
@@ -93,7 +93,28 @@ void giveMeTheTrail(DracView currentView, PlayerID player,
 LocationID *whereCanIgo(DracView currentView, int *numLocations, int road, int sea)
 {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return NULL;
+    // NOT FINISHED YET!!!!
+    // STILL WORKING ON IT!!!!
+    assert(currentView != NULL);
+    assert(numLocations != NULL);
+    
+    LocationID *whereCanIgo = malloc(sizeof(LocationID) * NUM_MAP_LOCATIONS);
+    int numOfLoc = 0;
+    whereCanIgo[numOfLoc] = 0;
+    VList curr;
+    while(curr != NULL){
+         if(road == TRUE && curr->type == ROAD){
+              assert(curr != ST_JOSEPH_AND_ST_MARYS);
+              whereCanIgo[numOfLoc++] = curr->v;
+         } else if(sea == TRUE && curr->type == SEA){
+              // lose 2 blood points
+              whereCanIgo[numOfLoc++] = curr->v;
+         }
+         curr = curr->next;
+    }
+    *numLocations = numOfLoc;
+    return whereCanIgo;
+//    return NULL;
 }
 
 // What are the specified player's next possible moves
@@ -101,5 +122,27 @@ LocationID *whereCanTheyGo(DracView currentView, int *numLocations,
                            PlayerID player, int road, int rail, int sea)
 {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-    return NULL;
+    // NOT FINISHED!!
+    // STILL WORKING ON IT!!!
+    assert(currentView != NULL);
+    assert(numLocations != NULL);
+    LocationID *whereCanTheyGo = malloc(sizeof(LocationID)*NUM_MAP_LOCATIONS);
+    int numOfLoc = 0;
+    whereCanTheyGo[numOfLoc] = 0;
+    int railMoves = (round + player) % 4;
+    VList curr;
+    while(curr != NULL){
+         if(road == TRUE && curr->type == ROAD){
+               whereCanTheyGo[numOfLoc++] = curr->v;
+          } else if(rail == TRUE && curr->type == RAIL){
+               // check for railmoves
+               whereCanTheyGo[numOfLoc++] = curr->v;
+          } else if(sea == TRUE && curr->type == SEA){
+               whereCanTheyGo[numOfLoc++] = curr->v;
+          }
+          curr = curr->next;
+    }
+    *numLocations = numOfLoc;
+    //return NULL;
+    return whereCanTheyGo;
 }
