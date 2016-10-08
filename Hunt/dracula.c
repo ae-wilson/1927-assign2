a3ac5bb6e59711fd6ee648a09253579011f5991d
Kelvin FAN <comp192716s2@gmail.com>
2016-10-08 15:43:18 +1100

    Updates

--- Hunt/dracula.c
+++ Hunt/dracula.c
@@ -633,10 +633,6 @@ static LocationID awayFromHunters(DracView gameState) {
 
     }
 
-    int i = 0;
-    printf("\n\nS Moves: \n");
-    for(i = 0; i < numSL; i++) printf("-> %s\n", idToName(safeLoc[i]));
-    printf("\n");
 
     free(safeLoc);
 
